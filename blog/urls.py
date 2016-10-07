6d9742dac14902de95f792a1c03dbda7fc71a1a4
HJ <hj@HJui-MacBook-Pro.local>
2016-10-08 01:10:56 +0900

    신규, 저장 , 수정 기능 구현

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,17 @@
 from django.conf.urls import url
 from . import views
 
-
 urlpatterns = [
+
+	# 블로그 목록
     url(r'^$', views.post_list, name='post_list'),
 
+    # 블로그 상세화면
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+
+    # 블로그 입력 
+    url(r'^post/new/$', views.post_new, name='post_new'),
+
+    # 블로그 수정
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.utils import timezone
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import Post
-
+from .forms import PostForm
 
 # lte : less than equal		<=
 # lt : less than			<
@@ -11,3 +11,40 @@ from .models import Post
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts' : posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog.views.post_detail', post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+
